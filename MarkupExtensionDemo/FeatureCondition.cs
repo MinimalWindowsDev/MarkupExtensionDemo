1e6b57589ae6cf9f2c3dd35d5a6124c8333d8bfe
F. S. Farimani <foad.farimani@nts-group.nl>
2024-11-28 15:29:04 +0100

    Add project files.

---
+++ MarkupExtensionDemo/FeatureCondition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace MarkupExtensionDemo
+{
+    public class FeatureCondition : MarkupExtension
+    {
+        public object WhenEnabled { get; set; }
+        public object WhenDisabled { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+#if fix_issue_001
+                return WhenEnabled;
+#else
+            return WhenDisabled;
+#endif
+        }
+    }
+}
---
+++ MarkupExtensionDemo/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace MarkupExtensionDemo
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private int _clickCount;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int ClickCount
+        {
+            get => _clickCount;
+            set
+            {
+                _clickCount = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ClickCount)));
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+#if fix_issue_001
+                ClickCount += 2;
+                MessageBox.Show($"Incremented by 2! New count: {ClickCount}");
+#else
+            ClickCount += 1;
+            MessageBox.Show($"Incremented by 1! New count: {ClickCount}");
+#endif
+        }
+    }
+}
